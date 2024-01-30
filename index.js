75e54fcfd790d36430be0a4a251cfcc6ffcc2ed1
developer-nieem <naimhossenwork@gmail.com>
2024-01-30 21:32:01 +0600

    Create CRUD operation for Event Item and Recent EVENT

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 
-//  
+//
 
-
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.xifd9dy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,25 +26,105 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const eventServicesCollection = client
+      .db("event360")
+      .collection("services");
+    const eventItemCollection = client.db("event360").collection("event");
+    const recentEventCollection = client
+      .db("event360")
+      .collection("recentEvent");
 
-    const eventServicesCollection = client.db("event360").collection("services")
-
-
-    app.get("/services", async(req,res) =>{
+    app.get("/services", async (req, res) => {
       const result = await eventServicesCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/event-item", async (req, res) => {
+      const result = await eventItemCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/recent-event", async (req, res) => {
+      const result = await recentEventCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/event-item", async (req, res) => {
+      const items = req.body;
+      const result = await eventItemCollection.insertOne(items);
+      res.send(result);
+    });
+
+    app.put("/event-item/:id", async (req, res) => {
+      const id = req.params.id;
+      const body = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          name: body.name,
+          image: body.image,
+        },
+      };
+      const result = await eventItemCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/event-item/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+
+      const result = await eventItemCollection.deleteOne(filter);
+
+      res.send(result);
+    });
+
+    // Recent Event 
+
+    app.post("/recent-event" , async (req, res) => {
+
+      const body = req.body;
+      const result =  await recentEventCollection.insertOne(body);
       res.send(result)
     })
 
-  //   app.post("/services" , async(req, res) => {
-  //     const items =  req.body;
-  //     const result =  await eventServicesCollection.insertOne(items);
-  //     res.send(result)
-  // })
+
+    app.put("/recent-event/:id" , async(req, res) => {
+
+      const body =  req.body;
+      const id = req.params.id;
+
+      const filter = {_id : new ObjectId(id)}
+
+      const updateDoc = {
+        $set: {
+          eventName : body.eventName,
+          organizerName : body.organizerName,
+          image : body.image
+        }
+      }
+
+      const result = await recentEventCollection.updateOne(filter, updateDoc)
+
+    })
+
+
+    app.delete("/recent-event/:id" , async(req, res) => {
+
+      const id =  req.params.id;
+      const filter = {_id : new ObjectId(id)}
+      const result =  await recentEventCollection.deleteOne(filter);
+
+
+      res.send(result)
+    })
+
 
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -55,15 +133,10 @@ async function run() {
 
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
-
+  console.log(`Example app listening on port ${port}`);
+});
